3518ee4a4fc330729d1e0a46c23f455bab8625e8
Benjamin Clark <benjaminclark@Benjamins-MBP-2.attlocal.net>
2021-05-22 12:16:45 -0500

    May 22 working session updates

--- app/templates/static/js/charts.js
+++ app/templates/static/js/charts.js
@@ -6,6 +6,7 @@ function init() {
     d3.csv("https://raw.githubusercontent.com/joelapsansky/red-team/main/Excel%20%26%20CSV/reshaped_full_cleaned_dataset.csv", function(data) {
 
       stats = Object.keys(data[0])
+      stats.pop()
   
       stats.forEach((stat) => {
         selector
@@ -13,191 +14,120 @@ function init() {
           .text(stat)
           .property("value", stat);
       });
-    });
+
       // Use the first sample from the list to build the initial plots
       var firststat = "SG_approach_green_AVERAGE";
       buildCharts(firststat);
-      // buildMetadata(firstSample);
-    };
-  
+
+      // var firstyear = "2021";
+      // buildCharts(firstyear);
+    });
+}
   // Initialize the dashboard
-  init();
+init();
+
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  // buildMetadata(newSample);
+  buildCharts(newSample);
   
-  function optionChanged(newSample) {
-    // Fetch new data each time a new sample is selected
-    // buildMetadata(newSample);
-    buildCharts(newSample);
-    
-  }
+}
   
   // Demographics Panel 
-  function buildMetadata(sample) {
-    d3.json("samples.json").then((data) => {
-      var metadata = data.metadata;
-      // Filter the data for the object with the desired sample number
-      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-      var result = resultArray[0];
-     
-      // Use d3 to select the panel with id of `#sample-metadata`
-      var PANEL = d3.select("#sample-metadata");
-  
-      // Use `.html("") to clear any existing metadata
-      PANEL.html("");
-  
-      // Use `Object.entries` to add each key and value pair to the panel
-      // Hint: Inside the loop, you will need to use d3 to append new
-      // tags for each key-value in the metadata.
-      Object.entries(result).forEach(([key, value]) => {
-        PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
-      });
-  
-    });
-  }
-  
-  // Create the buildChart function.
-  function buildCharts(golfstat) {
-    // Use d3.json to load the samples.json file 
-    d3.csv("https://raw.githubusercontent.com/joelapsansky/red-team/main/Excel%20%26%20CSV/reshaped_full_cleaned_dataset.csv", function(data) {
-      console.log(data);
-  
-      var yvalues = data.map(golfdata => golfdata[golfstat])
+  // function buildMetadata(sample) {
+  //   d3.json("samples.json").then((data) => {
+  //     var metadata = data.metadata;
+  //     // Filter the data for the object with the desired sample number
+  //     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+  //     var result = resultArray[0];
 
-      var xvalues = data.map(golfdata => golfdata["money_MONEY"])
+  //     // Use d3 to select the panel with id of `#sample-metadata`
+  //     var PANEL = d3.select("#sample-metadata");
+  
+  //     // Use `.html("") to clear any existing metadata
+  //     PANEL.html("");
+  
+  //     // Use `Object.entries` to add each key and value pair to the panel
+  //     // Hint: Inside the loop, you will need to use d3 to append new
+  //     // tags for each key-value in the metadata.
+  //     Object.entries(result).forEach(([key, value]) => {
+  //       PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+  //     });
+  
+  //   });
+  // }
+  
+// Create the buildChart function.
+function buildCharts(golfstat) {
+  // Use d3.json to load the samples.json file 
+  d3.csv("https://raw.githubusercontent.com/joelapsansky/red-team/main/Excel%20%26%20CSV/reshaped_full_cleaned_dataset.csv", function(data) {
+    console.log(data);
 
-      var trace2 = {
-        x: xvalues,
-        y: yvalues,
-        type: 'scatter'
-      };
-      
-      var barData = [trace2];
+    var data2017 = data.filter(golfdata => golfdata["Year"]==2017)
 
-    // //   // Create a variable that holds the samples array. 
+    var data2018 = data.filter(golfdata => golfdata["Year"]==2018)
 
-    // //   // Create a variable that filters the samples for the object with the desired sample number.
-    // //   var sampleArray = samples.filter(sampleObj => sampleObj.id == sample);
-    // //   // 1. Create a variable that filters the metadata array for the object with the desired sample number.
-    // //   var metadata = data.metadata;
-    // //   var metaArray = metadata.filter(sampleObj => sampleObj.id == sample);
-    // //   // Create a variable that holds the first sample in the array.
+    var data2019 = data.filter(golfdata => golfdata["Year"]==2019)
+
+    var data2020 = data.filter(golfdata => golfdata["Year"]==2020)
+
+    var data2021 = data.filter(golfdata => golfdata["Year"]==2021)
     
-    // //   var result = sampleArray[0];
-    // //   // 2. Create a variable that holds the first sample in the metadata array.
-    // //   var result1 = metaArray[0];
-  
-    // //   // Create variables that hold the otu_ids, otu_labels, and sample_values.
-    // //   var otu_ids = result.otu_ids
-    // //   var otu_labels = result.otu_labels
-    // //   var sample_values = result.sample_values;
-    // //   console.log(otu_ids)
-    // //   console.log(otu_labels)
-    // //   console.log(sample_values)
-  
-    // //   // 3. Create a variable that holds the washing frequency.
-    // //  var washing = parseFloat(result1.wfreq)
-    // //  console.log(washing)
-  
-    // //   // Create the yticks for the bar chart.
-    // //   // Hint: Get the the top 10 otu_ids and map them in descending order 
-    // //   // so the otu_ids with the most bacteria are last. 
-    // //   var yticks = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();
-  
-    //   // Create the trace for the bar chart. 
-    //   var barData = [ {
-    //     x: sample_values.slice(0,10).reverse(),
-    //     y: yticks,
-    //     text: otu_labels,
-    //     type:'bar',
-    //     orientation: 'h'
-        
-  
-    //   }
-        
-    //   ];
-    //   // Create the layout for the bar chart. 
-    //   var barLayout = {width: 375, height: 370, margin: { t: 95, b: 85},       
-    //   title: "Top 10 Bacterial Species",
-    //   xaxis: {title: "Sample Values"},
-    //   yaxis: {title: "ID's"}
-     
-    //   };
-  
-      // Use Plotly to plot the data with the layout. 
-      Plotly.newPlot("bar",barData);
-      // Create the trace for the bubble chart.
-      // var bubbleData = [{
-      //   x: otu_ids,
-      //   y: sample_values,
-      //   text: otu_labels,
-      //   mode: 'markers',
-      //   marker: {
-      //     size: sample_values,
-      //     color: otu_ids,
-      //     colorscale: 'Earth'
-      //   }
-        
-      // }
-     
-    //   ];
-  
-    //   // Create the layout for the bubble chart.
-    //   var bubbleLayout = {
-    //     title: "Top 10 Bacterial Species",
-    //     xaxis: {title: "Sample Values"},
-    //     yaxis: {title: "ID's"}
-        
-    //   };
-  
-    //   // D2: 3. Use Plotly to plot the data with the layout.
-    //   Plotly.newPlot("bubble",bubbleData, bubbleLayout)
-      
-    //   // 4. Create the trace for the gauge chart.
-    // //   var gaugeData = [{
-    // //     domain:{x: [0, 1], y: [0, 1] },
-    // //     value: washing,
-    // //     type: "indicator",
-    // //     mode: "gauge + number",
-    // //     //gauge: {
-    // //       //axis: { range: [null, 10], tickwidth: 1, tickcolor: "darkblue" }//,
-    // //    // title: { text: "Belly Button Washing Frequency", font: { size: 24 } },
-    // // //  }
-    // // }
-       
-    // //   ];
-      
-    // //   // 5. Create the layout for the gauge chart.
-    // //   var gaugeLayout = { 
-    // //     width: 500,
-    // //     height: 400,
-    // //     margin: { t: 25, r: 25, l: 25, b: 25 }//,
-    // //     //paper_bgcolor: "lavender",
-    // //     //font: { color: "darkblue", family: "Arial" }
-    // //   };
-  
-    // var gaugeData = [
-    //   {
-    //     domain: { x: [0, 1], y: [0, 1] },
-    //     value: washing,
-    //     gauge: {
-    //       axis: {range : [null,10],tickwidth:1},
-    //       steps: [
-    //         { range: [0, 2], color: "red" },
-    //         { range: [2, 4], color: "orange" },
-    //         { range: [4, 6], color: "yellow" },
-    //         { range: [6, 8], color: "limegreen" },
-    //         { range: [8, 10], color: "green" }]
-    //     },
-    //     title: { text: "Belly Button Washing Frequency" },
-    //     type: "indicator",
-    //     mode: "gauge+number"
-  
-    //   }
-    // ];
+    var yvalues2017 = data2017.map(golfdata => golfdata["money_MONEY"])
+
+    var xvalues2017 = data2017.map(golfdata => golfdata[golfstat])
+
+    var playername2017 = data2017.map(golfdata => golfdata["PLAYER NAME"])
+
+    var yvalues2018 = data2018.map(golfdata => golfdata["money_MONEY"])
+
+    var xvalues2018 = data2018.map(golfdata => golfdata[golfstat])
+
+    var playername2018 = data2018.map(golfdata => golfdata["PLAYER NAME"])
+
+    var yvalues2019 = data2019.map(golfdata => golfdata["money_MONEY"])
+
+    var xvalues2019 = data2019.map(golfdata => golfdata[golfstat])
+
+    var playername2019 = data2019.map(golfdata => golfdata["PLAYER NAME"])
+
+
+    var trace1 = {
+      x: xvalues2018,
+      y: yvalues2018,
+      mode: 'markers',
+      type: 'scatter',
+      text: playername2018,
+      name: "2018"
+    };
+
+    var trace2 = {
+      x: xvalues2017,
+      y: yvalues2017,
+      mode: 'markers',
+      type: 'scatter',
+      text: playername2017,
+      name: "2017"
+    };
     
-    // var gaugeLayout = { width: 400, height: 300, margin: { t: 0, b: 0} };
-  
-  
-    //   // 6. Use Plotly to plot the gauge data and layout.
-    //   Plotly.newPlot("gauge",gaugeData,gaugeLayout)
-    });
-  }
+    var scatterData = [trace1, trace2];
+
+    var layout = {
+      xaxis: {title: golfstat},
+      yaxis: {title: "Annual Winnings (millions $)"},
+      title:'Relationship between Performance Stat and Winnings',
+      legend: {
+        y: 0.5,
+        yref: 'paper',
+        font: {
+          family: 'Arial, sans-serif',
+          size: 20,
+          color: 'grey',
+        }
+    }
+    };
+
+    // Use Plotly to plot the data with the layout. 
+    Plotly.newPlot("scatter",scatterData, layout);
+  });
+}
