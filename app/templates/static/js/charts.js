c84e384365a68344c4ecfbf62aa3d1193be71a61
Joe Lapsansky <joe.lapsansky@gmail.com>
2021-05-22 12:45:12 -0500

    adding splice and rest of years to chart

--- app/templates/static/js/charts.js
+++ app/templates/static/js/charts.js
@@ -1,13 +1,13 @@
 function init() {
+
     // Grab a reference to the dropdown select element
     var selector = d3.select("#selDataset");
   
     // Use the list of sample names to populate the select options
     d3.csv("https://raw.githubusercontent.com/joelapsansky/red-team/main/Excel%20%26%20CSV/reshaped_full_cleaned_dataset.csv", function(data) {
 
-      stats = Object.keys(data[0])
-      stats.pop()
-  
+      var stats = Object.keys(data[0]).splice(1,22)
+      
       stats.forEach((stat) => {
         selector
           .append("option")
@@ -18,51 +18,28 @@ function init() {
       // Use the first sample from the list to build the initial plots
       var firststat = "SG_approach_green_AVERAGE";
       buildCharts(firststat);
-
-      // var firstyear = "2021";
-      // buildCharts(firstyear);
     });
-}
-  // Initialize the dashboard
+};
+
+// Initialize the dashboard
 init();
 
 function optionChanged(newSample) {
+
   // Fetch new data each time a new sample is selected
   // buildMetadata(newSample);
   buildCharts(newSample);
   
-}
-  
-  // Demographics Panel 
-  // function buildMetadata(sample) {
-  //   d3.json("samples.json").then((data) => {
-  //     var metadata = data.metadata;
-  //     // Filter the data for the object with the desired sample number
-  //     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-  //     var result = resultArray[0];
-
-  //     // Use d3 to select the panel with id of `#sample-metadata`
-  //     var PANEL = d3.select("#sample-metadata");
-  
-  //     // Use `.html("") to clear any existing metadata
-  //     PANEL.html("");
-  
-  //     // Use `Object.entries` to add each key and value pair to the panel
-  //     // Hint: Inside the loop, you will need to use d3 to append new
-  //     // tags for each key-value in the metadata.
-  //     Object.entries(result).forEach(([key, value]) => {
-  //       PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
-  //     });
-  
-  //   });
-  // }
-  
+};
+   
 // Create the buildChart function.
 function buildCharts(golfstat) {
+
   // Use d3.json to load the samples.json file 
   d3.csv("https://raw.githubusercontent.com/joelapsansky/red-team/main/Excel%20%26%20CSV/reshaped_full_cleaned_dataset.csv", function(data) {
     console.log(data);
 
+    // Filter 5 years
     var data2017 = data.filter(golfdata => golfdata["Year"]==2017)
 
     var data2018 = data.filter(golfdata => golfdata["Year"]==2018)
@@ -72,7 +49,8 @@ function buildCharts(golfstat) {
     var data2020 = data.filter(golfdata => golfdata["Year"]==2020)
 
     var data2021 = data.filter(golfdata => golfdata["Year"]==2021)
-    
+
+    // New x, y, and player name variables for trace
     var yvalues2017 = data2017.map(golfdata => golfdata["money_MONEY"])
 
     var xvalues2017 = data2017.map(golfdata => golfdata[golfstat])
@@ -91,8 +69,47 @@ function buildCharts(golfstat) {
 
     var playername2019 = data2019.map(golfdata => golfdata["PLAYER NAME"])
 
+    var yvalues2020 = data2020.map(golfdata => golfdata["money_MONEY"])
 
-    var trace1 = {
+    var xvalues2020 = data2020.map(golfdata => golfdata[golfstat])
+
+    var playername2020 = data2020.map(golfdata => golfdata["PLAYER NAME"])
+
+    var yvalues2021 = data2021.map(golfdata => golfdata["money_MONEY"])
+
+    var xvalues2021 = data2021.map(golfdata => golfdata[golfstat])
+
+    var playername2021 = data2021.map(golfdata => golfdata["PLAYER NAME"])
+
+    // Make 5 traces
+    var trace2021 = {
+      x: xvalues2021,
+      y: yvalues2021,
+      mode: 'markers',
+      type: 'scatter',
+      text: playername2021,
+      name: "2021"
+    };
+
+    var trace2020 = {
+      x: xvalues2020,
+      y: yvalues2020,
+      mode: 'markers',
+      type: 'scatter',
+      text: playername2020,
+      name: "2020"
+    };
+
+    var trace2019 = {
+      x: xvalues2019,
+      y: yvalues2019,
+      mode: 'markers',
+      type: 'scatter',
+      text: playername2019,
+      name: "2019"
+    };
+
+    var trace2018 = {
       x: xvalues2018,
       y: yvalues2018,
       mode: 'markers',
@@ -100,8 +117,8 @@ function buildCharts(golfstat) {
       text: playername2018,
       name: "2018"
     };
-
-    var trace2 = {
+    
+    var trace2017 = {
       x: xvalues2017,
       y: yvalues2017,
       mode: 'markers',
@@ -110,8 +127,10 @@ function buildCharts(golfstat) {
       name: "2017"
     };
     
-    var scatterData = [trace1, trace2];
+    // Define legend (and filters) for scatter plot
+    var scatterData = [trace2021, trace2020, trace2019, trace2018, trace2017];
 
+    // Define layout
     var layout = {
       xaxis: {title: golfstat},
       yaxis: {title: "Annual Winnings (millions $)"},
@@ -124,10 +143,10 @@ function buildCharts(golfstat) {
           size: 20,
           color: 'grey',
         }
-    }
+      }
     };
 
     // Use Plotly to plot the data with the layout. 
     Plotly.newPlot("scatter",scatterData, layout);
   });
-}
+};
